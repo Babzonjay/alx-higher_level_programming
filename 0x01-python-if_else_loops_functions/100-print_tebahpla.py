dbd928e1365f56fd99b07412e20bfa102407a37a
Babzonjay <babajide.osinusi@gmail.com>
2022-06-02 01:10:01 +0000

    Advanced fix

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for ascii_num in range(122, 96, -1):
-	if ascii_num % 2 == 1:
-		ascii_num = ascii_num - 32
-	print("{:c}".format(ascii_num), end='')
+for i in range(ord('z'), ord('a')-1, -1):
+    print('{:c}'.format(i) if i % 2 == 0 else chr(i-32), end='')
