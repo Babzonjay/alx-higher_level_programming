f3fa19b147bc0bf719cd76c8cf6a66da34890b64
Babzonjay <babajide.osinusi@gmail.com>
2022-06-22 00:09:34 +0000

    Fix tasks

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,15 @@
-s module defines a Square class and initialise its size
-"""
+re class definition"""
 
 
 class Square:
-    """Square implementation
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
     """
     def __init__(self, size):
+        """Initializes a square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
         self.__size = size
