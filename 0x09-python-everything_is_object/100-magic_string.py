87413fef868c746235fef38975123e064e6dbd64
Babzonjay <babajide.osinusi@gmail.com>
2022-06-29 23:07:55 +0000

    Submit 0x09-python

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,18 +1,4 @@
 #!/usr/bin/python3
-
-
-def call_counter(func):
-    def helper():
-        helper.calls += 1
-        return func()
-    helper.calls = 0
-
-    return helper
-
-
-@call_counter
-def magic_string():
-    return 'Holberton, ' * (magic_string.calls - 1) + 'Holberton'
-
-for i in range(10):
-    print(magic_string())
+def magic_string(H=[]):
+    H += ["Holberton"]
+    return (", ".join(H))
